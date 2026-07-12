19c5559f31758c4268d1771fc8e33fbe347e3cd1
sasi17 <sasidharreddy172737@gmail.com>
2026-07-12 15:18:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,105 @@
+import streamlit as st
+from scraper import attendance
+
+app = attendance()
+st.title("ATTENDANCE TRACKER")
+
+if "selected_sem" not in st.session_state:
+    st.session_state.selected_sem = None
+
+if "show_sem_form" not in st.session_state:
+    st.session_state.show_sem_form = False
+
+sem_options = ["SEM-1", "SEM-2", "SEM-3", "SEM-4", "SEM-5",
+               "SEM-6", "SEM-7", "SEM-8", "+ Add New Semester"]
+
+seme = st.selectbox("SELECT THE SEMESTER:", sem_options)
+
+if st.button("SUBMIT", key="submit_sem"):
+    if seme == "+ Add New Semester":
+        st.session_state.show_sem_form = True
+    else:
+        if seme not in app.Sem:
+            app.add_sem(seme)
+        st.session_state.selected_sem = seme
+        st.session_state.show_sem_form = False
+        st.success(f"WORKING ON {seme}")
+if st.session_state.show_sem_form:
+    custom_sem = st.text_input("ENTER YOUR SEM NAME:", key="custom_sem_input")
+    if st.button("ADD SEM", key="add_custom_sem"):
+        if custom_sem.strip() != "":
+            app.add_sem(custom_sem)
+            st.session_state.selected_sem = custom_sem
+            st.session_state.show_sem_form = False
+            st.success(f"ADDED YOUR SEM {custom_sem}")
+            st.rerun()
+        else:
+            st.error("ENTER THE SEM NAME")
+
+if st.session_state.selected_sem:
+    st.write(f"WORKING ON: {st.session_state.selected_sem}")
+if st.session_state.selected_sem:
+    sem_obj=app.Sem[st.session_state.selected_sem]
+    st.subheader(f"ADD THE SUBJECTS TO {st.session_state.selected_sem}")
+    new_sub_name=st.text_input("ENTER THE SUBJECT NAME:" ,key="new_sub_name")
+    new_sub_total=st.number_input("ENTER TOTAL NUMBER OF CLASSES:", min_value=1, step=1 ,key="new_sub_total")
+    if st.button("ADD SUBJECT",key="add_subject_btn"):
+        if new_sub_name.strip()!="":
+            sem_obj.add_subject(new_sub_name,new_sub_total)
+            app.save_data()
+            st.write(f"{new_sub_name} ADDED")
+            st.rerun
+        else:
+            st.error(f"ENTER A SUBJECT NAME")
+if st.session_state.selected_sem:
+    st.subheader("YOUR SUBJECTS")
+    if len(sem_obj.subj)==0:
+        st.write("NO SUBJECTS")
+    else:
+        
+        if st.button("MARK ALL"):
+            for sub_obj in sem_obj.subj.values():
+                sub_obj.mark_attendance("P")
+                app.save_data()
+            st.success("MARKED ALL")
+            st.rerun()
+        for sub_name ,sub_obj in sem_obj.subj.items():
+            with st.container(border=True):
+                st.write(f"{sub_name}")
+                pct=sub_obj.current_attendance()
+                st.write(f"ATTENDANCE:{pct:.1f}%")
+                st.write(sub_obj.get_status_message())
+                held = sub_obj.present_classes() + sub_obj.absent_classes()
+                st.write(f"HELD: {held} | REMAINING: {sub_obj.remaining_classes()}")
+                col1, col2, col3 ,col4= st.columns(4)
+                if col1.button("PRESENT", key=f"present-{sub_name}"):
+                    sub_obj.mark_attendance("P")
+                    app.save_data()
+                    st.rerun()
+
+                if col2.button("ABSENT", key=f"{sub_name}"):
+                    sub_obj.mark_attendance("A")
+                    app.save_data()
+                    st.rerun()
+
+                if col3.button("NO CLASS", key=f"noclass-{sub_name}"):
+                    sub_obj.mark_attendance("N")
+                    app.save_data()
+                    st.rerun()
+
+                if col4.button("DELECT SUBJECT", key=f"delete-{sub_name}"):
+                    del sem_obj.subj[sub_name]
+                    app.save_data()
+                    st.rerun()
+
+
+
+                
+
+
+
+
+        
+    
+
+
---
+++ scraper.py
@@ -0,0 +1,131 @@
+import json
+import os
+import datetime
+
+
+class subject:
+    def __init__(self, subject_name, total_classes):
+        self.sub = subject_name
+        self.total_class = total_classes
+        self.history = []
+
+    def mark_attendance(self, status):
+        date = str(datetime.date.today())
+        self.history.append({"STATUS": status, "DATE": date})
+
+    def notheld_classes(self):
+        return sum(1 for h in self.history if h["STATUS"] == "N")
+
+    def present_classes(self):
+        return sum(1 for h in self.history if h["STATUS"] == "P")
+
+    def absent_classes(self):
+        return sum(1 for h in self.history if h["STATUS"] == "A")
+
+    def remaining_classes(self):
+        return self.total_class - len(self.history)
+
+    def current_attendance(self):
+        ab = self.absent_classes()
+        pr = self.present_classes()
+        held = ab + pr
+        if held == 0:
+            return 0
+        return (pr / held) * 100
+
+    def max_skip_classes(self):
+        presentc = self.present_classes()
+        remainingc = self.remaining_classes()
+        max_skip = (remainingc + presentc) - (0.75 * self.total_class)
+        return max(0, int(max_skip))
+
+    def min_attend_classes(self):
+        presentc = self.present_classes()
+        remaining = self.remaining_classes()
+        need = (0.75 * self.total_class) - presentc
+        if need <= 0:
+            return 0
+        need = int(need) + 1 if need > int(need) else int(need)
+        return min(need, remaining)
+
+    def get_status_message(self):
+        pct = self.current_attendance()
+        if pct >= 75:
+            skip = self.max_skip_classes()
+            return f"SAFE-CAN SKIP {skip} MORE CLASSES"
+        else:
+            attend = self.min_attend_classes()
+            return f"WARNING-MUST ATTEND ALL {attend} CLASSES"
+
+    def to_dict(self):
+        return {
+            "name": self.sub,
+            "total_classes": self.total_class,
+            "history": self.history
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        s = cls(data["name"], data["total_classes"])
+        s.history = data["history"]
+        return s
+
+
+class semester:
+    def __init__(self, name):
+        self.name = name
+        self.subj = {}
+
+    def add_subject(self, name, total_cla):
+        self.subj[name] = subject(name, total_cla)
+
+    def mark_subject(self, subject_name, status):
+        if subject_name in self.subj:
+            self.subj[subject_name].mark_attendance(status)
+        else:
+            print("SUBJECT NOT FOUND")
+
+    
+
+    def to_dict1(self):
+        return {
+            "name": self.name,
+            "subjects": {name: s.to_dict() for name, s in self.subj.items()}
+        }
+
+    @classmethod
+    def from_dict1(cls, data1):
+        sem = cls(data1["name"])
+        for name, sub_data in data1["subjects"].items():
+            sem.subj[name] = subject.from_dict(sub_data)
+        return sem
+
+
+class attendance:
+    def __init__(self, file="attendance_data.json"):
+        self.file = file
+        self.Sem = {}
+        self.load_data()
+
+    def add_sem(self, name):
+        if name not in self.Sem:
+            self.Sem[name] = semester(name)
+            self.save_data()
+
+    def save_data(self):
+        data = {name: sem.to_dict1() for name, sem in self.Sem.items()}
+        with open(self.file, "w") as f:
+            json.dump(data, f, indent=2)
+
+    def load_data(self):
+        if os.path.exists(self.file):
+            try:
+                with open(self.file, "r") as f:
+                    content = f.read().strip()
+                    if content == "":
+                        return
+                    data = json.loads(content)
+                for name, sem_data in data.items():
+                    self.Sem[name] = semester.from_dict1(sem_data)
+            except json.JSONDecodeError:
+                print("Attendance file was empty or corrupted — starting fresh")
